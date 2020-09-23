6334e144b4d3baeb09324e5ac7e389e6ba1d4f4b
Hannes Jämtner <hanja189@student.liu.se>
2020-09-23 21:17:47 +0200

    feat: save and load functionallity, next add timer and moves t o save

--- scripts/LevelSelect.gd
+++ scripts/LevelSelect.gd
@@ -1,20 +1,17 @@
-extends Button
+extends Node2D
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	for level in global.data.levels:
+		get_node(level).get_child(0).text = "Moves: " + global.data.levels[level][0]
+		get_node(level).get_child(1).text = "Time: " + global.data.levels[level][1]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
-func _on_Start_pressed():
-	get_tree().change_scene("res://scenes/Scene1.tscn")
---
+++ scripts/global.gd
@@ -0,0 +1,36 @@
+extends Node
+
+var data
+
+func load_save() -> Dictionary:
+	var f := File.new()
+	f.open("res://save.json", File.READ)
+	var result := JSON.parse(f.get_as_text())
+	f.close()
+
+	if result.error:
+		save({
+		"levels": {}}
+		)
+		result.result = {"levels": {}}
+	print("SUCCESS LOAD")
+	return result.result as Dictionary
+
+func save(data: Dictionary):
+	var f := File.new()
+	f.open("res://save.json", File.WRITE)
+	prints("Saving to", f.get_path_absolute())
+	f.store_string(JSON.print(data))
+	print("CREATED SAVE FILE")
+	f.close()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	data = load_save()
+	print(data)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
